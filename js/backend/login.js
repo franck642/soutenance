e45a868b296ef490f19943018f47605f3de8678d
dassifr <136333316+dassifr@users.noreply.github.com>
2024-08-10 13:09:26 +0000

    design

--- js/backend/login.js
+++ js/backend/login.js
@@ -38,3 +38,25 @@
         }
     });
 }); 
+
+
+
+// function getCookie(name) {
+//     let cookieValue = null;
+//     if (document.cookie && document.cookie !== '') {
+//         const cookies = document.cookie.split(';');
+//         for (let i = 0; i < cookies.length; i++) {
+//             const cookie = cookies[i].trim();
+//             // Si le nom du cookie correspond au début de la chaîne
+//             if (cookie.substring(0, name.length + 1) === (name + '=')) {
+//                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+//                 break;
+//             }
+//         }
+//     }
+//     return cookieValue;
+// }
+
+// // Exemple d'utilisation :
+// const myCookieValue = getCookie('nom_du_cookie');
+// console.log(myCookieValue);
